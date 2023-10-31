1e9225becb3c2542a522e25d6cfae18525f62971
Behzad Rouhani <behxad.rowhani80.official@gmail.com>
2023-11-01 01:26:32 +0330

    make sidebar dynamicly active by helper method

--- app/Helper/helpers.php
+++ app/Helper/helpers.php
@@ -48,3 +48,13 @@ function deleteFileIfExist($filePath)
         throw $e;
     }
 }
+
+function setActive(array $route){
+    if(is_array($route)){
+        foreach($route as $r){
+            if(request()->routeIs($r)){
+                return 'active';
+            }
+        }
+    }
+}
