d61a1fe77f2fe3035dc821b845e7d9e43208c215
Behzad Rouhani <behxad.rowhani80.official@gmail.com>
2024-02-01 22:25:24 +0330

    making admin dashboard by showing necessary data dynamic

--- app/Http/Controllers/Backend/AdminController.php
+++ app/Http/Controllers/Backend/AdminController.php
@@ -2,13 +2,85 @@
 
 namespace App\Http\Controllers\Backend;
 
-use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use App\Models\User;
+use App\Models\Order;
+use App\Models\Vendor;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Models\ProductReview;
+use Illuminate\Support\Carbon;
+use App\Http\Controllers\Controller;
+use App\Models\NewsLetterSubscriber;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
-    public function dashboard(){
-        return view('admin.dashboard');
+    public function dashboard()
+    {
+        $todayOrder = Order::whereDate('created_at' , Carbon::today())->count();
+
+        $todayPendingOrder = Order::whereDate('created_at' , Carbon::today())
+        ->where('order_status' , 'pending')->count();
+
+        $totalOrder = Order::count();
+
+        $totalPendingOrder = Order::where('order_status' , 'pending')->count();
+
+        $totalCompletedOrder = Order::where('order_status' , 'delivered')->count();
+
+        $totalCanceledOrder = Order::where('order_status' , 'canceled')->count();
+
+        $totalProduct = Product::count();
+
+        $totalBlogs = Blog::count();
+
+        $todayEarning = Order::whereDate('created_at' , Carbon::today())
+        ->where('order_status' , '!=' , 'canceled')
+        ->sum('sub_total');
+
+        $thisMonthEarning = Order::whereMonth('created_at' , Carbon::now()->month)->where('order_status' , '!=' , 'canceled')
+        ->sum('sub_total');
+
+        $thisYearEarning = Order::whereYear('created_at' , Carbon::now()->year)->where('order_status' , '!=' , 'canceled')->sum('sub_total');
+
+        $totalEarning = Order::where('order_status' , '!=' , 'canceled')->sum('sub_total');
+
+        $pendingReviews = ProductReview::where('is_approved' , 0)->count();
+
+        $totalActiveReview = ProductReview::where('is_approved' , 1)->count();
+
+        $totalActiveUser = User::where(['status' => 'active' , 'role' => 'user'])->count();
+        $totalActiveVendor = User::where(['status' => 'active' , 'role' => 'vendor'])->count();
+        $totalActiveAdmin = User::where(['status' => 'active' , 'role' => 'admin'])->count();
+        $banUsers = User::where('status' , 'inactive')->count();
+        $requestToBeVendor = Vendor::where('status' , 0)->count();
+
+        $subscribers = NewsLetterSubscriber::where('is_verified' , 1)->count();
+
+
+        return view('admin.dashboard' , compact(
+            'todayOrder',
+            'todayPendingOrder',
+            'totalOrder',
+            'totalPendingOrder',
+            'totalCompletedOrder',
+            'totalCanceledOrder',
+            'totalProduct',
+            'totalBlogs',
+            'todayEarning',
+            'thisMonthEarning',
+            'thisYearEarning',
+            'totalEarning',
+            'pendingReviews',
+            'totalActiveReview',
+            'totalActiveUser',
+            'totalActiveVendor',
+            'totalActiveAdmin',
+            'banUsers',
+            'requestToBeVendor',
+            'subscribers'
+        ));
     }
 
     public function login(){
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,47 +6,300 @@
       <h1>Dashboard</h1>
     </div>
     <div class="row">
+        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+            <a href="{{route('admin.customer.index')}}">
+
+                <div class="card card-statistic-1">
+                  <div class="card-icon bg-secondary">
+                    <i class="fas fa-user"></i>
+                  </div>
+                  <div class="card-wrap">
+                    <div class="card-header">
+                      <h4>Active Users</h4>
+                    </div>
+                    <div class="card-body">
+                      {{$totalActiveUser}}
+                    </div>
+                  </div>
+                </div>
+            </a>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+            <a href="{{route('admin.vendor.index')}}">
+
+                <div class="card card-statistic-1">
+                  <div class="card-icon bg-secondary">
+                    <i class="fas fa-user"></i>
+                  </div>
+                  <div class="card-wrap">
+                    <div class="card-header">
+                      <h4>Active Vendor</h4>
+                    </div>
+                    <div class="card-body">
+                      {{$totalActiveVendor}}
+                    </div>
+                  </div>
+                </div>
+            </a>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+            <a href="{{route('admin.admin-list.index')}}">
+
+                <div class="card card-statistic-1">
+                  <div class="card-icon bg-secondary">
+                    <i class="fas fa-user"></i>
+                  </div>
+                  <div class="card-wrap">
+                    <div class="card-header">
+                      <h4>Active Admin</h4>
+                    </div>
+                    <div class="card-body">
+                      {{$totalActiveAdmin}}
+                    </div>
+                  </div>
+                </div>
+            </a>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+            <div class="card card-statistic-1">
+              <div class="card-icon bg-danger">
+                <i class="fas fa-ban"></i>
+              </div>
+              <div class="card-wrap">
+                <div class="card-header">
+                  <h4>Ban Users</h4>
+                </div>
+                <div class="card-body">
+                  {{$banUsers}}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+            <a href="{{route('admin.vendor-request.index')}}">
+
+                <div class="card card-statistic-1">
+                  <div class="card-icon bg-info">
+                    <i class="fas fa-circle"></i>
+                  </div>
+                  <div class="card-wrap">
+                    <div class="card-header">
+                      <h4>Request to become vendor</h4>
+                    </div>
+                    <div class="card-body">
+                      {{$requestToBeVendor}}
+                    </div>
+                  </div>
+                </div>
+            </a>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+            <a href="{{route('admin.product.index')}}">
+
+                <div class="card card-statistic-1">
+                  <div class="card-icon bg-info">
+                    <i class="fas fa-shopping-bag"></i>
+                  </div>
+                  <div class="card-wrap">
+                    <div class="card-header">
+                      <h4>Total Products</h4>
+                    </div>
+                    <div class="card-body">
+                      {{$totalProduct}}
+                    </div>
+                  </div>
+                </div>
+            </a>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+            <a href="{{route('admin.blog.index')}}">
+
+                <div class="card card-statistic-1">
+                  <div class="card-icon bg-info">
+                    <i class="far fa-file"></i>
+                  </div>
+                  <div class="card-wrap">
+                    <div class="card-header">
+                      <h4>Total Blogs</h4>
+                    </div>
+                    <div class="card-body">
+                      {{$totalBlogs}}
+                    </div>
+                  </div>
+                </div>
+            </a>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+            <a href="{{route('admin.subscriber.index')}}">
+
+                <div class="card card-statistic-1">
+                  <div class="card-icon bg-info">
+                    <i class="far fa-newspaper"></i>
+                  </div>
+                  <div class="card-wrap">
+                    <div class="card-header">
+                      <h4>News Letter Subscriber</h4>
+                    </div>
+                    <div class="card-body">
+                      {{$subscribers}}
+                    </div>
+                  </div>
+                </div>
+            </a>
+          </div>
       <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-        <div class="card card-statistic-1">
-          <div class="card-icon bg-primary">
-            <i class="far fa-user"></i>
-          </div>
-          <div class="card-wrap">
-            <div class="card-header">
-              <h4>Total Admin</h4>
+        <a href="{{route('admin.order.index')}}">
+
+            <div class="card card-statistic-1">
+              <div class="card-icon bg-primary">
+                <i class="far fa-shopping-cart"></i>
+              </div>
+              <div class="card-wrap">
+                <div class="card-header">
+                  <h4>Today's Orders</h4>
+                </div>
+                <div class="card-body">
+                    {{$todayOrder}}
+                </div>
+              </div>
             </div>
-            <div class="card-body">
-              10
+        </a>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <a href="{{route('admin.order-pending')}}">
+
+            <div class="card card-statistic-1">
+              <div class="card-icon bg-primary">
+                <i class="far fa-shopping-cart"></i>
+              </div>
+              <div class="card-wrap">
+                <div class="card-header">
+                  <h4>Todays Pending Orders</h4>
+                </div>
+                <div class="card-body">
+                  {{$todayPendingOrder}}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+        </a>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <a href="{{route('admin.order.index')}}">
+
+            <div class="card card-statistic-1">
+              <div class="card-icon bg-primary">
+                <i class="far fa-shopping-cart"></i>
+              </div>
+              <div class="card-wrap">
+                <div class="card-header">
+                  <h4>Total Orders</h4>
+                </div>
+                <div class="card-body">
+                  {{$totalOrder}}
+                </div>
+              </div>
+            </div>
+        </a>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <a href="{{route('admin.order-pending')}}">
+
+            <div class="card card-statistic-1">
+              <div class="card-icon bg-primary">
+                <i class="far fa-shopping-cart"></i>
+              </div>
+              <div class="card-wrap">
+                <div class="card-header">
+                  <h4>Total Pending Orders</h4>
+                </div>
+                <div class="card-body">
+                  {{$totalPendingOrder}}
+                </div>
+              </div>
+            </div>
+        </a>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <a href="{{route('admin.order-delivered')}}">
+
+            <div class="card card-statistic-1">
+              <div class="card-icon bg-primary">
+                <i class="far fa-shopping-cart"></i>
+              </div>
+              <div class="card-wrap">
+                <div class="card-header">
+                  <h4>Total Complete Orders</h4>
+                </div>
+                <div class="card-body">
+                  {{$totalCompletedOrder}}
+                </div>
+              </div>
+            </div>
+        </a>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <a href="{{route('admin.order-canceled')}}">
+
+            <div class="card card-statistic-1">
+              <div class="card-icon bg-danger">
+                <i class="far fa-shopping-cart"></i>
+              </div>
+              <div class="card-wrap">
+                <div class="card-header">
+                  <h4>Total Canceled Orders</h4>
+                </div>
+                <div class="card-body">
+                  {{$totalCanceledOrder}}
+                </div>
+              </div>
+            </div>
+        </a>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <a href="{{route('admin.review.index')}}">
+            <div class="card card-statistic-1">
+            <div class="card-icon bg-warning">
+                <i class="far fa-star"></i>
+            </div>
+            <div class="card-wrap">
+                <div class="card-header">
+                <h4>Pending Reviews</h4>
+                </div>
+                <div class="card-body">
+                {{$pendingReviews}}
+                </div>
+            </div>
+            </div>
+        </a>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <a href="{{route('admin.review.index')}}">
+            <div class="card card-statistic-1">
+              <div class="card-icon bg-warning">
+                <i class="far fa-star"></i>
+              </div>
+              <div class="card-wrap">
+                <div class="card-header">
+                  <h4>Active Reviews</h4>
+                </div>
+                <div class="card-body">
+                  {{$totalActiveReview}}
+                </div>
+              </div>
+            </div>
+        </a>
       </div>
       <div class="col-lg-3 col-md-6 col-sm-6 col-12">
         <div class="card card-statistic-1">
-          <div class="card-icon bg-danger">
-            <i class="far fa-newspaper"></i>
+          <div class="card-icon bg-success">
+            <i class="fas fa-money-bill"></i>
           </div>
           <div class="card-wrap">
             <div class="card-header">
-              <h4>News</h4>
+              <h4>Today's Earnigs</h4>
             </div>
             <div class="card-body">
-              42
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-        <div class="card card-statistic-1">
-          <div class="card-icon bg-warning">
-            <i class="far fa-file"></i>
-          </div>
-          <div class="card-wrap">
-            <div class="card-header">
-              <h4>Reports</h4>
-            </div>
-            <div class="card-body">
-              1,201
+              {{$settings->currency_icon}}{{$todayEarning}}
             </div>
           </div>
         </div>
@@ -54,14 +307,44 @@
       <div class="col-lg-3 col-md-6 col-sm-6 col-12">
         <div class="card card-statistic-1">
           <div class="card-icon bg-success">
-            <i class="fas fa-circle"></i>
+            <i class="fas fa-money-bill"></i>
           </div>
           <div class="card-wrap">
             <div class="card-header">
-              <h4>Online Users</h4>
+              <h4>This Month Earnigs</h4>
             </div>
             <div class="card-body">
-              47
+              {{$settings->currency_icon}}{{$thisMonthEarning}}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <div class="card card-statistic-1">
+          <div class="card-icon bg-success">
+            <i class="fas fa-money-bill"></i>
+          </div>
+          <div class="card-wrap">
+            <div class="card-header">
+              <h4>This Year Earnigs</h4>
+            </div>
+            <div class="card-body">
+              {{$settings->currency_icon}}{{$thisYearEarning}}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <div class="card card-statistic-1">
+          <div class="card-icon bg-success">
+            <i class="fas fa-money-bill"></i>
+          </div>
+          <div class="card-wrap">
+            <div class="card-header">
+              <h4>Total Earnigs</h4>
+            </div>
+            <div class="card-body">
+              {{$settings->currency_icon}}{{$totalEarning}}
             </div>
           </div>
         </div>
